b04ee17ca07c80ea88eb466889a65a09fae63f92
gayfulina <irinmdr@gmail.com>
2021-01-22 12:24:17 -0800

    history

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+//Core
+import React, {useEffect, useState} from "react";
+import {Router, Route, Link} from "react-router-dom";
+import Registration from "./components/Registration";
+import Login from "./components/Login";
+import CV from "./components/CV";
+import history from './history';
+//Styles
+import styles from "./App.module.css";
+import classnames from "classnames";
 
-function App() {
+const App = () => {
+  const initialButtonsState = JSON.parse(sessionStorage.getItem("buttons")) || [
+    {name: 'Registration', isClicked: true, path: '/'},
+    {name: 'Login', isClicked: false, path: '/Login'},
+    {name: 'CV', isClicked: false, path: '/CV'}
+  ];
+  const [buttons, setButtons] = useState(initialButtonsState);
+
+  useEffect(() => {
+    sessionStorage.setItem("buttons", JSON.stringify(buttons));
+  },[]);
+
+  const chooseButtonClick = (idx) => {
+    const newButtons = [...buttons];
+    newButtons.map(el => el.isClicked = false);
+    newButtons[idx].isClicked = true;
+    setButtons(newButtons);
+  };
+
+  let linkList = buttons.map((el, i) => {
+    return <Link
+        key={el.path}
+        to={el.path}
+        className={classnames({
+          [styles.link]: true,
+          [styles.linkActive]: (el.isClicked),
+        })}
+        onClick={() => chooseButtonClick(i)}
+    >{el.name}</Link>
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router history={history}>
+        <div className={styles.wrap}>
+          <div className={styles.inner}>
+            <div className={styles.nav}>
+              {linkList}
+            </div>
+            {/*<div className={styles.content}>*/}
+            {/*<Switch>*/}
+            <Route path='/' exact component={Registration}/>
+            <Route path='/Login' component={Login}/>
+            <Route path='/CV' component={CV}/>
+            {/*</Switch>*/}
+            {/*</div>*/}
+          </div>
+        </div>
+      </Router>
   );
 }
 
